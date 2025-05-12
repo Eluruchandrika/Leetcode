47f20dd40e511103da866dc30b21ffd72961c253
Eluruchandrika <152140974+Eluruchandrika@users.noreply.github.com>
2025-05-12 14:14:22 +0530

    Time: 0 ms (100%), Space: 40.2 MB (91.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,22 +1,9 @@
 class Solution {
+    int [] memo=new int[46];
     public int climbStairs(int n) {
-         int[] memo = new int[n+1];
-        return fibHelper(n,memo);
-        
-    }
-
-    private int fibHelper(int n, int[] memo){
-        if(n<=2){
-            return n;
-        }
-
-        if(memo[n] != 0){
-            return memo[n];
-        }
-
-        memo[n] = fibHelper(n-1,memo)+ fibHelper(n-2,memo);
+        if (n<=3) return n;
+        if(memo[n] != 0) return memo[n];
+        memo[n] = climbStairs(n-1)+climbStairs(n-2);
         return memo[n];
-
-        
     }
 }
